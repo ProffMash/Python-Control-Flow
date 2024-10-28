65166262d765de2cff1391dc2ecaf575ec58303f
david <kdavidmacharia8485@gmail.com>
2024-10-28 11:25:12 +0300

    added quiz 3

---
+++ question_3.py
@@ -0,0 +1,10 @@
+def sum_of_list(nums):
+    total = 0
+    for num in nums:
+        total += num
+    return total
+
+# Example usage
+nums = [1, 2, 3, 4, 5]
+result = sum_of_list(nums)
+print("The sum of the list is:", result)
